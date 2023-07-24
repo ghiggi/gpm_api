fb0d33a2d655428a6031dbf58210476e7a8012e6
Evan Thomas <evan.thomas@epfl.ch>
2023-07-24 15:09:04 +0200

    Mock ftplib, add tests to disk, download, info

--- gpm_api/tests/conftest.py
+++ gpm_api/tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import datetime
 from typing import Any
 from gpm_api.io.products import get_info_dict, available_products
 
@@ -66,15 +67,71 @@ def password() -> str:
 
 
 @pytest.fixture
-def server_paths() -> list[str]:
+def server_paths() -> dict[str, dict[str, Any]]:
     """Return a list of probable GPM server paths"""
 
-    return [  # Not validated to be real paths but follow the structure
-        "ftps://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S170044-E183317.036092.V07A.HDF5",
-        "ftps://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S183318-E200550.036093.V07A.HDF5",
-        "ftps://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S200551-E213823.036094.V07A.HDF5",
+    # Not validated to be real paths but follow the structure
+    return {
+        "ftps://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S170044-E183317.036092.V07A.HDF5": {
+            "year": 2020,
+            "month": 7,
+            "day": 5,
+            "product_category": "radar",
+            "product_type": "RS",
+            "start_time": datetime.datetime(2020, 7, 5, 17, 0, 44),
+            "end_time": datetime.datetime(2020, 7, 5, 18, 33, 17),
+            "version": 7,
+        },
+        "ftps://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S183318-E200550.036093.V07A.HDF5": {
+            "year": 2020,
+            "month": 7,
+            "day": 5,
+            "product_category": "radar",
+            "product_type": "RS",
+            "start_time": datetime.datetime(2020, 7, 5, 18, 33, 18),
+            "end_time": datetime.datetime(2020, 7, 5, 20, 5, 50),
+            "version": 7,
+        },
+        "ftps://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S200551-E213823.036094.V07A.HDF5": {
+            "year": 2020,
+            "month": 7,
+            "day": 5,
+            "product_category": "radar",
+            "product_type": "RS",
+            "start_time": datetime.datetime(2020, 7, 5, 20, 5, 51),
+            "end_time": datetime.datetime(2020, 7, 5, 21, 38, 23),
+            "version": 7,
+        },
         # Include non-ftps folders
-        "ftp://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S213824-E231056.036095.V07A.HDF5",
-        "ftp://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S231057-E004329.036096.V07A.HDF5",
-        "ftp://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S004330-E021602.036097.V07A.HDF5",
-    ]
+        "ftp://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S213824-E231056.036095.V07A.HDF5": {
+            "year": 2020,
+            "month": 7,
+            "day": 5,
+            "product_category": "radar",
+            "product_type": "RS",
+            "start_time": datetime.datetime(2020, 7, 5, 21, 38, 24),
+            "end_time": datetime.datetime(2020, 7, 5, 23, 10, 56),
+            "version": 7,
+        },
+        "ftp://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S231057-E004329.036096.V07A.HDF5": {
+            "year": 2020,
+            "month": 7,
+            "day": 5,
+            "product_category": "radar",
+            "product_type": "RS",
+            "start_time": datetime.datetime(2020, 7, 5, 23, 10, 57),
+            "end_time": datetime.datetime(2020, 7, 6, 0, 43, 29),
+            "version": 7,
+        },
+        "ftp://arthurhouftps.pps.eosdis.nasa.gov/gpmdata/2020/07/05/radar/2A.GPM.DPR.V9-20211125.20200705-S004330-E021602.036097.V07A.HDF5": {
+            "year": 2020,
+            "month": 7,
+            "day": 5,
+            "product_category": "radar",
+            "product_type": "RS",
+            "start_time": datetime.datetime(2020, 7, 5, 0, 43, 30),
+            "end_time": datetime.datetime(2020, 7, 5, 2, 16, 2),
+            "version": 7,
+        },
+        # TODO: Add more products with varying attributes ...
+    }
--- gpm_api/tests/io/test_download.py
+++ gpm_api/tests/io/test_download.py
@@ -1,7 +1,11 @@
 import pytest
 import os
 import datetime
+import ftplib
+from typing import Any
+from pytest_mock.plugin import MockerFixture
 from gpm_api.io import download as dl
+from gpm_api.io.products import available_products
 
 
 def test_construct_curl_cmd(
@@ -106,3 +110,131 @@ def test_construct_wget_cmd(
         assert os.path.exists(
             os.path.dirname(disk_path)
         ), f"Folder {os.path.dirname(disk_path)} was not created"
+
+
+# May be better to test child functions... (get_disk_directory, get_start_time_from_filepaths)
+
+# def test_convert_pps_to_disk_filepaths(
+#     server_paths: list[str],
+#     products: list[str],
+#     product_categories: list[str],
+#     product_types: list[str],
+#     versions: list[int],
+#     tmpdir: str,
+# ) -> None:
+#     """Test that the PPS filepaths are correctly converted to disk filepaths"""
+
+#     base_dir = os.path.join(
+#         tmpdir,
+#         datetime.datetime.utcnow().isoformat(),
+#     )
+
+#     # for product_type in product_types:
+#     #     for product in available_products(product_type=product_type):
+#     #         for version in versions:
+#     disk_filepaths = dl.convert_pps_to_disk_filepaths(
+#         pps_filepaths=server_paths,
+#         base_dir=base_dir,
+#         product=product,
+#         product_type=product_type,
+#         version=version,
+#     )
+#     for i, server_path in enumerate(server_paths):
+#         # Remote URI scheme
+#         split_no_uri = server_path.split("://")[-1]
+
+#         _uri, _gpm, y, m, d, categ, file = split_no_uri.split("/")
+
+#         # Assertions assume the list order is identical
+#         if product_type == "RS":
+#             assert disk_filepaths[i] == os.path.join(
+#                 base_dir,
+#                 "GPM",
+#                 product_type,
+#                 f"V0{version}",
+#                 categ,
+#                 product,
+#                 y,
+#                 m,
+#                 d,
+#                 file,
+#             )
+#         elif product_type == "NRT":
+#             assert disk_filepaths[i] == os.path.join(
+#                 base_dir,
+#                 "GPM",
+#                 product_type,
+#                 categ,
+#                 product,
+#                 y,
+#                 m,
+#                 d,
+#                 file,
+#             )
+
+# def _download_with_ftlib(server_path, disk_path, username, password):
+#     # Infer hostname
+#     hostname = server_path.split("/", 3)[2]  # remove ftps:// and select host
+
+#     # Remove hostname from server_path
+#     server_path = server_path.split("/", 3)[3]
+
+#     # Connect to the FTP server using FTPS
+#     ftps = ftplib.FTP_TLS(hostname)
+
+#     # Login to the FTP server using the provided username and password
+#     ftps.login(username, password)  # /gpmdata base directory
+
+#     # Download the file from the FTP server
+#     try:
+#         with open(disk_path, "wb") as file:
+#             ftps.retrbinary(f"RETR {server_path}", file.write)
+#     except EOFError:
+#         return f"Impossible to download {server_path}"
+
+#     # Close the FTP connection
+#     ftps.close()
+#     return None
+
+
+def test_download_with_ftplib(
+    mocker: MockerFixture,
+    server_paths: dict[str, dict[str, Any]],
+    username: str,
+    password: str,
+    tmpdir: str,
+) -> None:
+    ftp_mock = mocker.patch(
+        "ftplib.FTP_TLS",
+        autospec=True,
+    )
+    file_open_mock = mocker.patch("builtins.open", autospec=True)
+
+    # ftp_mock_instance = mocker.Mock(spec=FTP_TLS)
+    ftp_mock.login.return_value = "230 Login successful."
+    # Set the return value for the retrbinary method to indicate a successful download
+    ftp_mock.retrbinary.return_value = "226 Transfer complete."
+    ftp_mock.retrbinary.side_effect = [
+        "226 Transfer complete.",
+    ]
+
+    disk_paths = []
+    for server_path in server_paths:
+        disk_paths.append(
+            os.path.join(
+                tmpdir,
+                "test_download_with_ftplib",
+                os.path.basename(server_path),
+            )
+        )
+
+    dl.ftplib_download(
+        server_paths=server_paths.keys(),
+        disk_paths=server_paths,
+        username=username,
+        password=password,
+    )
+
+    assert ftp_mock.called
+
+    # TODO: Assert username/password are passed to ftp_mock.login (assert_called_with not working?)
