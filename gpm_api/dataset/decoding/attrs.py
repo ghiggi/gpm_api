d08acaca23f8415bb078f44c6b01e1ca7a1e9dd5
ghiggi <gionata.ghiggi@gmail.com>
2023-08-02 14:10:31 +0200

    Fix warning related to bad _FillValue and CodeMissingValue in HDF5 files

--- gpm_api/dataset/decoding/attrs.py
+++ gpm_api/dataset/decoding/attrs.py
@@ -6,15 +6,40 @@ Created on Fri Jul 28 13:50:59 2023
 """
 
 
+def convert_string_to_number(string):
+    if string.isdigit():
+        return int(string)
+    else:
+        return float(string)
+
+
 def _format_dataarray_attrs(da, product=None):
     attrs = da.attrs
 
+    # Ensure fill values are numbers
+    if "CodeMissingValue" in attrs:
+        if isinstance(attrs["CodeMissingValue"], str):
+            attrs["CodeMissingValue"] = convert_string_to_number(attrs["CodeMissingValue"])
+    if "_FillValue" in attrs:
+        if isinstance(attrs["_FillValue"], str):
+            attrs["_FillValue"] = convert_string_to_number(attrs["_FillValue"])
+
+    # Check _FillValue and CodeMissingValue agrees
+    # - Do not since _FillValue often badly defined !
+    # - TODO: report issues to NASA team
+    # if "_FillValue" in attrs  and "CodeMissingValue" in attrs:
+    #     if attrs["_FillValue"] != attrs["CodeMissingValue"]:
+    #         name = da.name
+    #         fillvalue = attrs["_FillValue"]
+    #         codevalue = attrs["CodeMissingValue"]
+    #         raise ValueError(f"In {name}, _FillValue is {fillvalue} and CodeMissingValue is {codevalue}")
+
     # Convert CodeMissingValue' to _FillValue if available
-    if not attrs.get("_FillValue", False) and attrs.get("CodeMissingValue", False):
-        attrs["_FillValue"] = attrs.pop("CodeMissingValue")
+    if "CodeMissingValue" in attrs:
+        attrs["_FillValue"] = attrs["CodeMissingValue"]
 
     # Remove 'CodeMissingValue'
-    attrs.pop("CodeMissingValue", None)
+    _ = attrs.pop("CodeMissingValue", None)
 
     # Convert 'Units' to 'units'
     if not attrs.get("units", False) and attrs.get("Units", False):
