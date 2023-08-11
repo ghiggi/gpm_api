95123f834a612d918fb3f761d17019fd7873676c
ghiggi <gionata.ghiggi@gmail.com>
2023-08-11 16:55:27 +0200

    Add timing decorator

--- gpm_api/utils/timing.py
+++ gpm_api/utils/timing.py
@@ -6,7 +6,7 @@ Created on Tue Jul 25 19:25:07 2023
 """
 import datetime
 import time
-
+from time import perf_counter
 
 def print_elapsed_time(fn):
     def decorator(*args, **kwargs):
@@ -17,5 +17,21 @@ def print_elapsed_time(fn):
         timedelta_str = str(datetime.timedelta(seconds=execution_time))
         print(f"Elapsed time: {timedelta_str} .", end="\n")
         return results
+
+    return decorator
+
+
+def print_task_elapsed_time(prefix=" - "):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = perf_counter()
+            results = func(*args, **kwargs)
+            end_time = perf_counter()
+            execution_time = end_time - start_time
+            timedelta_str = str(datetime.timedelta(seconds=execution_time))
+            print(f"{prefix} Elapsed time: {timedelta_str} .", end="\n")
+            return results
+
+        return wrapper
 
     return decorator
