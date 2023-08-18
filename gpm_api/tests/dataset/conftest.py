fee9d884945a43e01b1c0f9e5ded15a20718ecd7
Evan Thomas <evan.thomas@epfl.ch>
2023-08-18 07:45:16 +0200

    Attempt to load dataset from local resources without success

--- gpm_api/tests/dataset/conftest.py
+++ gpm_api/tests/dataset/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 import h5py
 import os
+from gpm_api import open_dataset
+import xarray as xr
 
 
 @pytest.fixture
-def sample_dataset():
+def sample_dataset() -> xr.Dataset:
     """Return a sample dataset to use for testing
 
     Dataset is a 2A DPR file from 2022-07-05 that has been generated with
@@ -12,20 +14,25 @@ def sample_dataset():
 
     """
 
-    return h5py.File(
-        os.path.join(
-            os.getcwd(),
-            "gpm_api",
-            "tests",
-            "resources",
-            "GPM",
-            "RS",
-            "V07",
-            "RADAR",
-            "2A-DPR",
-            "2022",
-            "07",
-            "05",
-            "2A.GPM.DPR.V9-20211125.20220705-S144632-E161905.047447.V07A.HDF5",
-        )
+    # os.path.join(
+    #     os.getcwd(),
+    #     "gpm_api",
+    #     "tests",
+    #     "resources",
+    #     "GPM",
+    #     "RS",
+    #     "V07",
+    #     "PMW",
+    #     "1C-MHS-METOPB",
+    #     "2020",
+    #     "08",
+    #     "01",
+    #     "1C.METOPB.MHS.XCAL2016-V.20200801-S102909-E121030.040841.V07A.HDF5",
+    # )
+    # )
+    ds = open_dataset(
+        start_time="2022-07-01T10:29:09",
+        end_time="2022-08-01T12:10:30",
+        product="2A-DPR",
     )
+    return ds
