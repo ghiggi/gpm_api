04dfabc3b499702a8b98c2b6557d914d97d7f058
ghiggi <gionata.ghiggi@gmail.com>
2023-11-15 14:38:22 +0100

    Fix retrievals import bug

--- gpm_api/retrievals/routines.py
+++ gpm_api/retrievals/routines.py
@@ -51,10 +51,10 @@ def available_retrievals(ds):
     product = _infer_product(ds)
     # Define retrievals for 2A-<RADAR> products
     if product in available_products(product_category="RADAR", product_level="2A"):
-        module_name = "gpm_api.dataset.retrievals.retrieval_2a_radar"
+        module_name = "gpm_api.retrievals.retrieval_2a_radar"
         return _get_available_retrievals(module_name)
     if product in available_products(product_category="PMW", product_level="2A"):
-        module_name = "gpm_api.dataset.retrievals.retrieval_2a_pmw"
+        module_name = "gpm_api.retrievals.retrieval_2a_pmw"
         return _get_available_retrievals(module_name)
 
 
@@ -79,10 +79,10 @@ def get_retrieval_variable(ds, retrieval, *args, **kwargs):
 
     # Define retrievals for 2A-<RADAR> products
     if product in available_products(product_category="RADAR", product_level="2A"):
-        module_name = "gpm_api.dataset.retrievals.retrieval_2a_radar"
+        module_name = "gpm_api.retrievals.retrieval_2a_radar"
         check_retrieval_validity(ds, retrieval)
         return _get_retrieval_function(module_name, retrieval)(ds, *args, **kwargs)
     if product in available_products(product_category="PMW", product_level="2A"):
-        module_name = "gpm_api.dataset.retrievals.retrieval_2a_pmw"
+        module_name = "gpm_api.^retrievals.retrieval_2a_pmw"
         check_retrieval_validity(ds, retrieval)
         return _get_retrieval_function(module_name, retrieval)(ds, *args, **kwargs)
