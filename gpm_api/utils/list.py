082856b3e7944641fae7b774fec880bd31398381
Son Pham-Ba <son.phamba@epfl.ch>
2023-11-23 09:46:55 +0100

    fix utils/list flatten_list

--- gpm_api/utils/list.py
+++ gpm_api/utils/list.py
@@ -13,12 +13,10 @@ Created on Thu Oct 26 18:09:38 2023
 def flatten_list(nested_list):
     """Flatten a nested list into a single-level list."""
 
+    if isinstance(nested_list, list) and len(nested_list) == 0:
+        return nested_list
     # If list is already flat, return as is to avoid flattening to chars
-    if (
-        isinstance(nested_list, list)
-        and len(nested_list) == 1
-        and not isinstance(nested_list[0], list)
-    ):
+    if isinstance(nested_list, list) and not isinstance(nested_list[0], list):
         return nested_list
     return (
         [item for sublist in nested_list for item in sublist]
