59a1b48c57dcfcb82d0219e7660a54ac7fd929ed
ghiggi <gionata.ghiggi@gmail.com>
2024-02-07 12:42:03 +0100

    Fix trim_memory for OS

--- gpm_api/utils/dask.py
+++ gpm_api/utils/dask.py
@@ -4,13 +4,22 @@ Created on Wed Aug  9 10:30:36 2023
 
 @author: ghiggi
 """
-
 import ctypes
+import platform
 
 
 def trim_memory() -> int:
-    libc = ctypes.CDLL("libc.so.6")
-    return libc.malloc_trim(0)
+    os_name = platform.system()
+    if os_name == "Linux":
+        libc = ctypes.CDLL("libc.so.6")
+        return libc.malloc_trim(0)
+    elif os_name == "Windows":
+        # Windows does not have a direct equivalent
+        pass
+    elif os_name == "Darwin":
+        # macOS (Darwin) does not have a direct equivalent
+        pass
+    return -1  # Indicate no operation was performed
 
 
 def clean_memory(client):
