62792b927700440093d8b1c55a59f09532d2dda1
ghiggi <gionata.ghiggi@gmail.com>
2024-06-26 12:17:13 +0200

    Ensure windows-python 3.12 compatibility

--- gpm/tests/test_dataset/test_coords.py
+++ gpm/tests/test_dataset/test_coords.py
@@ -95,9 +95,14 @@ def test_get_orbit_coords():
     returned_coords = {k: (list(da.dims), da.data) for k, da in returned_coords.items()}
 
     # Ensure same integer for gpm_granule_id
-    expected_coords["gpm_granule_id"][1] = expected_coords["gpm_granule_id"][1].astype(int)
-    returned_coords["gpm_granule_id"][1] = returned_coords["gpm_granule_id"][1].astype(int)
-
+    expected_coords["gpm_granule_id"] = (
+        expected_coords["gpm_granule_id"][0],
+        expected_coords["gpm_granule_id"][1].astype(int),
+    )
+    returned_coords["gpm_granule_id"] = (
+        returned_coords["gpm_granule_id"][0],
+        returned_coords["gpm_granule_id"][1].astype(int),
+    )
     # Compare same type
     diff = DeepDiff(expected_coords, returned_coords)
     assert diff == {}, f"Dictionaries are not equal: {diff}"
